832117fce1a2119b0645c678705a4c5f0326d82d
supriya-0406 <supriyabg86@gmail.com>
2025-07-01 18:29:31 +0530

    Add files via upload

---
+++ import cv2.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib.pyplot as plt
+# Read an image (make sure to upload an image to Colab or use a valid path)
+# You can upload an image using the following code:
+from google.colab import files
+uploaded = files.upload()
+# Assuming you uploaded an image named 'image.jpg'
+image = cv2.imread('python.jpg')
+# Display the image using Matplotlib
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(10, 5))
+plt.imshow(image)
+plt.axis('off')  # Hide axis
+plt.show()
